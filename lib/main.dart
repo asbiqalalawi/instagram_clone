e63ef5aaf1b96398609bac235fc68af624436684
Asbiq Al Alawi <asbiqalalawi@gmail.com>
2023-04-21 17:10:44 +0700

    Initial commit

---
+++ lib/main.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+import 'package:instagram_clone/utils/colors.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Instagram Clone',
+      theme: ThemeData.dark().copyWith(scaffoldBackgroundColor: mobileBackgroundColor),
+      home: const Scaffold(
+        body: Center(child: Text('Build Instagram Clone')),
+      ),
+    );
+  }
+}
