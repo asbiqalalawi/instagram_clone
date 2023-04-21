cc128e3026d2148fdb658e4e0adc25017b8ab73a
Asbiq Al Alawi <asbiqalalawi@gmail.com>
2023-04-21 20:44:09 +0700

    Setting Up Firebase (Android and Web)

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,24 @@
+import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:instagram_clone/responsive/mobile_screen_layout.dart';
 import 'package:instagram_clone/responsive/responsive_layout_screen.dart';
 import 'package:instagram_clone/responsive/web_screen_layout.dart';
 import 'package:instagram_clone/utils/colors.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  if (kIsWeb) {
+    await Firebase.initializeApp(
+      options: const FirebaseOptions(
+        apiKey: 'AIzaSyAB5vJzADmyhGWzpr_5CLsISuzhjQThR-8',
+        appId: '1:880701272105:web:1cf24266bcf7ef48796c38',
+        messagingSenderId: '880701272105',
+        projectId: 'instagram-clone-b15e4',
+      ),
+    );
+  }
+  await Firebase.initializeApp();
   runApp(const MyApp());
 }
 
@@ -17,7 +31,9 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       title: 'Instagram Clone',
-      theme: ThemeData.dark().copyWith(scaffoldBackgroundColor: Colors.amber),
+      theme: ThemeData.dark().copyWith(
+        scaffoldBackgroundColor: mobileBackgroundColor,
+      ),
       home: const ResponsiveLayout(
         mobileScreenLayout: MobileScreenLayout(),
         webScreenLayout: WebScreenLayout(),
